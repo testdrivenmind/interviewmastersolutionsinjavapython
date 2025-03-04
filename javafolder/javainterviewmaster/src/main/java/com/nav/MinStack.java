57b349bb60bd656c9d0f659cccb96dc230dae337
Navaneeth <zorbhathebuddha@gmail.com>
2025-03-04 22:23:58 +0800

    refactored minStack sol

--- javafolder/javainterviewmaster/src/main/java/com/nav/MinStack.java
+++ javafolder/javainterviewmaster/src/main/java/com/nav/MinStack.java
@@ -17,25 +17,6 @@ import java.util.Stack;
 
 public class MinStack {
 
-    //  have two array list, one for the order in which they came and one for storing the minimum value
-    //  insertionOrderList just add the element and push
-    //  for minOrderList, check the minValue before ordering and push the minValue to the last
-    //  when poping if i need to pop the top value from insertionOrderList and wherever that specific value is there in the minOrderList
-    // which cannot be o(1)
-    // so another option to solve, this will be to have the index, of the minValue stored in dict and remove that index, will it be considered
-    // o(1)
-    // or should we go with some other data strucute like linkedlist
-    // Input ["MinStack","push","push","push","getMin","pop","top","getMin"]
-    // Input [[],[-2],[0],[-3],[],[],[],[]]
-    // Output [null,null,null,null,-3,null,0,-2]
-
-
-    // mainStack = [-2, 0 (as this is needs to be there for pop), -3]
-    // minStack = [0,-2, -3 ]
-
-
-
-
     int val;
     List<Integer> mainStack = new ArrayList<>();
     Stack<Integer> minStack = new Stack<>();
@@ -45,16 +26,8 @@ public class MinStack {
      public void push(int val) {
         this.val = val;
         mainStack.add(val);
-        if (minStack.isEmpty()) {
-            minStack.add(val);
-        } else {
-            if (val < minStack.peek()) {
-                minStack.push(val);
-            } else {
-                minStack.push(minStack.peek());
-            }
-        }
-       
+        int minVal = Math.min(val, minStack.isEmpty() ? val : minStack.peek());
+        minStack.push(minVal);
     };
 
 
