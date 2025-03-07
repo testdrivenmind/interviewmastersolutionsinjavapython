e8d5ee30ad99c4b2fd7ffb457f42329e6dc1d948
Navaneeth <zorbhathebuddha@gmail.com>
2025-03-08 07:50:33 +0800

    set zero matrix sol in py

--- interviewmaster/set_matrix_zeros.py
+++ interviewmaster/set_matrix_zeros.py
@@ -1,9 +1,25 @@
 from typing import List
 
 
-# class Solution:
+class Solution:
 
-#     def set_zeros(self, matrix: List[List[int]]) -> None:
+    def set_zeros(self, matrix: List[List[int]]) -> None:
+        rows_to_mark_zero = set()
+        cols_to_mark_zero = set()
+    
+
+        for row_index, row_list in enumerate(matrix):
+            for col_index, value in enumerate(row_list):
+                if value == 0:
+                    rows_to_mark_zero.add(row_index)
+                    cols_to_mark_zero.add(col_index)
+
+        for row in rows_to_mark_zero:
+            matrix[row] = [0] * len(matrix[0])
+        
+        for col_index in cols_to_mark_zero:
+            for row_index in range(len(matrix)):
+                matrix[row_index][col_index] = 0
 
 
 
